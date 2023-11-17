f65d9d1ac9f66ce94d36e7e0cdcf1c0622ae45c6
Ayoub Benezzi <142675900+ayoubenezzi@users.noreply.github.com>
2023-11-17 10:46:23 +0100

    Guess my number
    🎲 Guess My Number Game 🧠
    
    Welcome to "Guess My Number," a simple yet exhilarating JavaScript project that brings the classic guessing game to life! With a combination of HTML, CSS, and JavaScript, I've created an interactive game that challenges you to guess the secret number.
    
    🚀 Features:
    
    User-Friendly Interface: The game features a clean and intuitive design, making it easy for players of all levels to jump in and start guessing.
    Dynamic Feedback: Receive instant feedback on your guesses. The game will let you know if your guess is too high, too low, or spot-on!
    Score Tracking: Challenge yourself to guess the number in the least number of attempts possible. Your score is tracked, adding an element of competition.
    🎯 How to Play:
    
    Enter your guess in the input field.
    Receive feedback on whether your guess is too high, too low, or correct.
    Keep refining your guesses until you crack the code!

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".message").textContent = "🎉 Correct Number";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
